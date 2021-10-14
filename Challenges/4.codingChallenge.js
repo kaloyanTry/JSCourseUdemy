d213a9549a4a8855f3be1e65960f8e527e624791
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-10-14 21:59:55 +0300

    Add files via upload

---
+++ Challenges/4.codingChallenge.js
@@ -0,0 +1,6 @@
+const bill = 275; //275, 40, 430
+
+const tip = bill >= 30 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(tip);
+console.log(`Thi bill is: ${bill} and the tip is: ${tip}`);
