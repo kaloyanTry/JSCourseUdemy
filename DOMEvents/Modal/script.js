ad99be2c00d949503d68b7cdd218d5c96c80f8e7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-10-28 21:41:01 +0300

    Add files via upload

---
+++ DOMEvents/Modal/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModels = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+    console.log("Button clicked");
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModels.length; i++) {
+    btnsOpenModels[i].addEventListener('click', openModal);
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+
+
