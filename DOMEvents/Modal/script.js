b3e44890641930a9bc0b70d3bdf3db3ba9c46490
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-10-29 21:35:05 +0300

    Update script.js

--- DOMEvents/Modal/script.js
+++ DOMEvents/Modal/script.js
@@ -6,23 +6,26 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModels = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-    console.log("Button clicked");
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+  //console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModels.length; i++) {
-    btnsOpenModels[i].addEventListener('click', openModal);
-};
+  btnsOpenModels[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-
-
-
+document.addEventListener('keydown', function (e) {
+  //console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
