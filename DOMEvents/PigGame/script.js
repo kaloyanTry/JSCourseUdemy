5c6fdbe166ed91ab1a8b0cc00f51e5d44a02072b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-10-30 08:45:12 +0300

    Add files via upload

--- DOMEvents/PigGame/script.js
+++ DOMEvents/PigGame/script.js
@@ -2,8 +2,29 @@
 
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
