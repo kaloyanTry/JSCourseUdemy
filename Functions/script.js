f41a2f6793575b73c444ec0b2927de8e1edc991d
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-11-12 10:51:07 +0200

    Add files via upload

--- Functions/script.js
+++ Functions/script.js
@@ -41,3 +41,41 @@
 // console.log(traveler); // traveler is already: Mr. Kaloyan Ganchev
 // awere of the issues: JS has only: passing by value, but NOT passing by reference
 // *********************
+
+// Functions accepting Callback functions:
+// Functions that accept other functions as input:
+
+// First function for replace and lowercase string /lower-order function/:
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+//Second function for capitalize first word /lower-order function/:
+const upperFirstWord = function (str) {
+  const [firstWord, ...otherWords] = str.split(' ');
+  return [firstWord.toUpperCase(), ...otherWords].join(' ');
+};
+
+// Function that accept f()s = Higher-order function:
+const transformerFunc = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+const inputString = 'Javascript is the Best!';
+
+// Calling the Higher-order function:
+transformerFunc(inputString, upperFirstWord);
+
+//upperFirstWord is just the value, NOT calling the function(). So, it is said a callback function
+
+transformerFunc(inputString, oneWord);
+// JS usees callbacks all the time!!!
+
+const giveSmile = function () {
+  console.log(':D');
+};
+document.body.addEventListener('click', giveSmile);
+['Kevi', 'Ani', 'Vesi'].forEach(giveSmile);
