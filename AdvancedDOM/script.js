0c4120bcc6980a233e357ce5d1f41fcd8267859b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-11-23 21:25:25 +0200

    Add files via upload

---
+++ AdvancedDOM/script.js
@@ -0,0 +1,204 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// Buttons scrolling:
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Delegation => smooth scroll link
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy in Delegation:
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabbed component === Manipulating the content =>
+// => add, remove classes:
+
+// use event Delegation:
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  // console.log(clicked);
+
+  if (!clicked) return; // modern way of returning function/not null object
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation: Intersection Observer API:
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+// Select elements:
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// const header = document.querySelector('.header');
+// const allSelectors = document.querySelectorAll('.section');
+// console.log(allSelectors);
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button'); // it gives HTMLCollection not a node list
+// console.log(allButtons);
+
+//////////////////////////////////////////
+// Creating and inserting elements:
+// .insertAdjacentHTML; // a popular way of creating element
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+
+// // message.textContent = 'We use cookied for improved functionality and analytics.';
+
+// message.innerHTML =
+//   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.append(message);
+
+// // header.prepend(message);
+// // header.before(message);
+// // header.after(message);
+
+// //////////////////////////////////////////
+// // Delete elements:
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+// ////////////////////////////////////////
+// // Styles:
+// message.style.backgroundColor = '#37383d'; //inline styles:
+// message.style.width = '120%';
+
+// // Attributes:
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt); // getAttribute
+// console.log(logo.src);
+
+// logo.setAttribute('company', 'Bankist'); // setAttribute
+// console.log(logo.getAttribute('company')); // getAttribute
+
+// // Classes:
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'j');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   //console.log(s1coords);
+
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+// const alertH1Func = function (e) {
+//   alert('addEventListener: Great! You are reading the heading');
+
+//   h1.removeEventListener('mouseenter', alertH1Func);
+// };
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', alertH1Func);
