678b731a91b52782607738bcd660a946378971e5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-11-27 12:56:31 +0200

    Update script.js

--- OOP/Chalange1/script.js
+++ OOP/Chalange1/script.js
@@ -1,53 +1,5 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  // console.log(this);
-  this.firstName = firstName; //Instance properties
-  this.birthYear = birthYear;
-
-  // Very bad practise to make methods in constructor function like this:
-  // this.calcAge = function () {
-  //   console.log(2022 - this.birthYear);
-  // };
-};
-
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-// **Creating an object:
-// 1. New empty object {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype: set properties
-// 4. function automatically return {}
-
-console.log(jonas instanceof Person);
-
-const veni = new Person('Veni', 1996);
-const gabi = new Person('Gabi', 2019);
-console.log(veni);
-console.log(gabi);
-
-// Create a method to Person: Inherit a method or Deligation:
-Person.prototype.calcAge = function () {
-  console.log(2021 - this.birthYear);
-};
-
-jonas.calcAge();
-veni.calcAge();
-gabi.calcAge();
-
-// Creating a property: Inherit a propery
-Person.prototype.species = 'Homo Sapiens';
-
-console.log(jonas.__proto__); //__proto__ = property of the instance jonas
-console.log(jonas.__proto__ === Person.prototype);
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 4, 5, 6, 9, 4];
-console.log(arr.__proto__);
-
-// -----------------------
-
 // Chalange 1:
 
 const Car = function (make, speed) {
