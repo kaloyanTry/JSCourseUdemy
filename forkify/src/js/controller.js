99de9abd5490a45dbe20f5e50eea996d7c54d6ca
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-12-11 21:33:41 +0200

    Add files via upload

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -1,9 +1,3 @@
-import * as model from './model.js';
-import recipeView from './views/recipeView.js';
-
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -18,26 +12,29 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const controlRecipes = async function () {
+const showRecipe = async function () {
   try {
-    const id = window.location.hash.slice(1);
-    //console.log(id);
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40`
+    );
+    const data = await res.json();
 
-    if (!id) return;
-    recipeView.renderSpinner();
+    if (!res.ok) throw new Error(`${data.mesage} (${res.staus})`);
 
-    // 1) Loading recipe:
-    await model.loadRecipe(id);
-
-    // 2) Render recipe:
-    recipeView.render(model.state.recipe);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
 };
-
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+showRecipe();
