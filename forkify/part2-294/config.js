d310ff6a9c4e9dd81025b66efc3976f110581747
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2021-12-22 21:10:10 +0200

    Update config.js

--- forkify/part2-294/config.js
+++ forkify/part2-294/config.js
@@ -1,4 +1,3 @@
 // all the varibles that will be constant in this project, so easy to config
-'use strict';
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes'; //Upercase letters that show that it NOT change
 export const TIMEOUT_SEC = 10;
